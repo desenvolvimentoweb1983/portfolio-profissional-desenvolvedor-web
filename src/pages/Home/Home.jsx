710c9a81b3298345cc5b2b59f97d79d7718915be
desenvolvimentoweb1983 <desenvolvimentoweb1983@gmail.com>
2026-04-27 20:13:52 -0300

    imagem match atualizada 3

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -26,7 +26,7 @@ function Home() {
       descricao: "Treinamento Interativo Sobre Atendimento ao Cliente.",
       url: "https://treinamentoatendimentocliente.vercel.app/",
       tecnologias: [<FaHtml5 />, <FaCss3Alt />, <FaJs />, <FaReact />],
-      imagem: "%2Fimages%2Flogo-match.png",
+      imagem: "https://treinamentoatendimentocliente.vercel.app/images/logo-match.png",
     },
     {
       nome: "WebDev Luís",
